7c944ede889d03d71de2637b700c93584a868522
arcan1s <esalexeev@gmail.com>
2014-06-15 16:09:53 +0400

    release 2.3.1 small changes in the daemon

--- PKGBUILD
+++ PKGBUILD
@@ -2,7 +2,7 @@
 # Maintainer: Evgeniy "arcanis" Alexeev <esalexeev@gmail.com>
 
 pkgname=git-etc
-pkgver=2.3.0
+pkgver=2.3.1
 pkgrel=1
 pkgdesc="Simple daemon for monitoring changes in files"
 arch=('i686' 'x86_64')
@@ -13,7 +13,7 @@ makedeps=('python2')
 optdepends=('python2-pyqt4: for GUI'
             'xterm: for GUI')
 source=(https://github.com/arcan1s/git-etc/releases/download/V.${pkgver}/${pkgname}-${pkgver}-src.tar.xz)
-md5sums=('b889107af9871d73830e72524ef20ec9')
+md5sums=('196e03491d7e1528fd09d02e15013ebc')
 backup=('etc/git-etc.conf')
 
 package() {
--- sources/ctrlconf.sh
+++ sources/ctrlconf.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #     git-etc simple daemon written on BASH for monitoring changes in files
 #     Copyright (C) 2013 Evgeniy Alekseev
-# 
+#
 #     This program is free software; you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
 #     the Free Software Foundation; either version 3 of the License, or
 #     (at your option) any later version.
-# 
+#
 #     This program is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
-# 
+#
 #     You should have received a copy of the GNU General Public License
 #     along with this program; if not, see http://www.gnu.org/licenses
 #     or write to the Free Software Foundation,Inc., 51 Franklin Street,
@@ -26,7 +26,7 @@ error_mes() {
 }
 func_help() {
   echo -e "GUI for git-etc daemon"
-  echo -e "\nUsage: ctrlconf [--default] [ -h | --help ] [ -v | --version ]" 
+  echo -e "\nUsage: ctrlconf [--default] [ -h | --help ] [ -v | --version ]"
   echo -e "\nAdditional parametrs:"
   echo -e "      --default     - create default configuration file"
   echo -e "  -h  --help        - show this help and exit"
@@ -37,7 +37,7 @@ func_help() {
 func_ver() {
   echo -e "                    Control Config"
   echo -e "GUI for git-etc daemon to work with GIT repository"
-  echo -e "Version : 2.3.0                    License : GPLv3"
+  echo -e "Version : 2.3.1                    License : GPLv3"
   echo -e "                                        by arcanis"
   echo -e "                      E-mail : esalexeev@gmail.com"
   exit 0
@@ -48,7 +48,7 @@ until [ -z $1 ]; do
   case "$1" in
     "-h" | "--help"    ) func_help;;
     "-v" | "--version" ) func_ver;;
-    "--default"        ) [ -f "$HOME/.config/ctrlconf.conf" ] && rm "$HOME/.config/ctrlconf.conf";;
+    "--default"        ) [ -f "${HOME}/.config/ctrlconf.conf" ] && rm "${HOME}/.config/ctrlconf.conf";;
     *                  ) error_mes "flag";;
   esac
   shift
